68e5f206b9d8513b7fff9fa4770809094f7721bb
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-11-06 21:57:37 +0000

    quick sort and small sum question

---
+++ src/sortAlgorithms/QuickSort.java
@@ -0,0 +1,133 @@
+package sortAlgorithms;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            swap(arr, left + (int) (Math.random() * (right - left + 1)), right);
+            int[] p = partition(arr, left, right);
+            quickSort(arr, left, p[0] - 1); // p[0] is the index of the first element of the = part
+            quickSort(arr, p[1] + 1, right); // p[1] is the index of the last element of the = part
+        }
+    }
+
+    /**
+     * partition
+     * separate the array into three parts: <, =, >
+     * @param arr array
+     * @param left left index
+     * @param right right index
+     * @return an array with two elements: the index of first and last element of the = part
+     */
+    private static int[] partition(int[] arr, int left, int right) {
+        int less = left - 1; // the index using to monitor the < part
+        int more = right; // the index using to monitor the > part
+        while (left < more) {
+            // if the current number is less than the number at the right index
+            // swap the current number with the number at the left index
+            if (arr[left] < arr[right]) {
+                swap(arr, ++less, left++);
+                // if the current number is greater than the number at the right index
+                // swap the current number with the number at the right index
+            } else if (arr[left] > arr[right]) {
+                swap(arr, --more, left);
+            } else {
+                left++;
+            }
+        }
+        swap(arr, more, right);
+        return new int[] {less + 1, more};
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j]= temp;
+    }
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            int[] arr3 = copyArray(arr1);
+            quickSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println("test end");
+        System.out.println(testTime + " tests: " + (succeed ? "succeed" : "failed"));
+    }
+}
