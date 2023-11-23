a03d889da900dd7778e1be02f6af7cec272970d5
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-11-23 23:32:30 +0000

    update dijkstra

--- src/graph/Dijkstra.java
+++ src/graph/Dijkstra.java
@@ -53,4 +53,114 @@ public class Dijkstra {
         }
         return minNode;
     }
+
+    public static class NodeRecord {
+        public Node node;
+        public int distance;
+
+        public NodeRecord(Node node, int distance) {
+            this.node = node;
+            this.distance = distance;
+        }
+    }
+
+    public static class NodeHeap {
+
+        private Node[] nodes;
+
+        private HashMap<Node, Integer> heapIndexMap;
+        private HashMap<Node, Integer> distanceMap;
+        private int size;
+
+        public NodeHeap(int size) {
+            nodes = new Node[size];
+            heapIndexMap = new HashMap<>();
+            distanceMap = new HashMap<>();
+            size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public void addOrUpdateOrIgnore(Node node, int distance) {
+            if (inHeap(node)) { // update
+                distanceMap.put(node, Math.min(distanceMap.get(node), distance));
+                insertHeapify(node, heapIndexMap.get(node));
+            }
+            if (!isEntered(node)) { // add
+                nodes[size] = node;
+                heapIndexMap.put(node, size);
+                distanceMap.put(node, distance);
+                insertHeapify(node, size++);
+            }
+            // ignore
+        }
+
+        public NodeRecord pop() {
+            NodeRecord nodeRecord = new NodeRecord(nodes[0], distanceMap.get(nodes[0]));
+            swap(0, size - 1); // 0 > size - 1    size - 1 > 0
+            heapIndexMap.put(nodes[size - 1], -1);
+            distanceMap.remove(nodes[size - 1]);
+            // free C++同学还要把原本堆顶节点析构，对java同学不必
+            nodes[size - 1] = null;
+            heapify(0, --size);
+            return nodeRecord;
+        }
+
+        private void insertHeapify(Node node, int index) {
+            while (distanceMap.get(nodes[index]) < distanceMap.get(nodes[(index - 1) / 2])) {
+                swap(index, (index - 1) / 2);
+                index = (index - 1) / 2;
+            }
+        }
+
+        private void heapify(int index, int size) {
+            int left = index * 2 + 1;
+            while (left < size) {
+                int smallest = left + 1 < size && distanceMap.get(nodes[left + 1]) < distanceMap.get(nodes[left])
+                        ? left + 1
+                        : left;
+                smallest = distanceMap.get(nodes[smallest]) < distanceMap.get(nodes[index]) ? smallest : index;
+                if (smallest == index) {
+                    break;
+                }
+                swap(smallest, index);
+                index = smallest;
+                left = index * 2 + 1;
+            }
+        }
+
+        private boolean isEntered(Node node) {
+            return heapIndexMap.containsKey(node);
+        }
+
+        private boolean inHeap(Node node) {
+            return isEntered(node) && heapIndexMap.get(node) != -1;
+        }
+
+        private void swap(int index1, int index2) {
+            heapIndexMap.put(nodes[index1], index2);
+            heapIndexMap.put(nodes[index2], index1);
+            Node tmp = nodes[index1];
+            nodes[index1] = nodes[index2];
+            nodes[index2] = tmp;
+        }
+    }
+
+    public static HashMap<Node, Integer> dijkstra2(Node head, int size) {
+        NodeHeap nodeHeap = new NodeHeap(size);
+        nodeHeap.addOrUpdateOrIgnore(head, 0);
+        HashMap<Node, Integer> result = new HashMap<>();
+        while (!nodeHeap.isEmpty()) {
+            NodeRecord record = nodeHeap.pop();
+            Node cur = record.node;
+            int distance = record.distance;
+            for (Edge edge : cur.edges) {
+                nodeHeap.addOrUpdateOrIgnore(edge.to, edge.weight + distance);
+            }
+            result.put(cur, distance);
+        }
+        return result;
+    }
 }
