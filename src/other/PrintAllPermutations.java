b452299f24bf21f25829784c86d6cbb06b74e40f
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-11-26 22:59:56 +0000

    print all permutations

---
+++ src/other/PrintAllPermutations.java
@@ -0,0 +1,109 @@
+package other;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintAllPermutations {
+
+    public static List<String> permutation1(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.isEmpty()) {
+            return ans;
+        }
+        char[] str = s.toCharArray();
+        ArrayList<Character> rest = new ArrayList<>();
+        for (char cha : str) {
+            rest.add(cha);
+        }
+        String path = "";
+        process1(rest, path, ans);
+        return ans;
+    }
+
+    private static void process1(ArrayList<Character> rest, String path, List<String> ans) {
+        if (rest.isEmpty()) {
+            ans.add(path);
+        } else {
+            int N = rest.size();
+            for (int i = 0; i < N; i++) {
+                char cur = rest.get(i);
+                rest.remove(i);
+                process1(rest, path + cur, ans);
+                rest.add(i, cur);
+            }
+        }
+    }
+
+    public static List<String> permutation2(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.isEmpty()) {
+            return ans;
+        }
+        char[] str = s.toCharArray();
+        process2(str, 0, ans);
+        return ans;
+    }
+
+    private static void process2(char[] str, int index, List<String> ans) {
+        if (index == str.length) {
+            ans.add(String.valueOf(str));
+        } else {
+            for (int i = index; i < str.length; i++) {
+                swap(str, index, i);
+                process2(str, index + 1, ans);
+                swap(str, index, i);
+            }
+        }
+    }
+
+    public static List<String> permutation3(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.isEmpty()) {
+            return ans;
+        }
+        char[] str = s.toCharArray();
+        process3(str, 0, ans);
+        return ans;
+    }
+
+    private static void process3(char[] str, int index, List<String> ans) {
+        if (index == str.length) {
+            ans.add(String.valueOf(str));
+        } else {
+            boolean[] visited = new boolean[26];
+            for (int i = index; i < str.length; i++) {
+                if (!visited[str[i] - 'a']) {
+                    visited[str[i] - 'a'] = true;
+                    swap(str, index, i);
+                    process3(str, index + 1, ans);
+                    swap(str, index, i);
+                }
+            }
+        }
+    }
+
+    private static void swap(char[] chs, int i, int j) {
+        char tmp = chs[i];
+        chs[i] = chs[j];
+        chs[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        String s = "acc";
+        List<String> ans1 = permutation1(s);
+        for (String str : ans1) {
+            System.out.println(str);
+        }
+        System.out.println("=======");
+        List<String> ans2 = permutation2(s);
+        for (String str : ans2) {
+            System.out.println(str);
+        }
+        System.out.println("=======");
+        List<String> ans3 = permutation3(s);
+        for (String str : ans3) {
+            System.out.println(str);
+        }
+
+    }
+}
