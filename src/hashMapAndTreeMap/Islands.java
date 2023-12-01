a251b0b4c307e02cf4591613e1f17dafa2b76140
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-12-01 03:21:47 +0000

    union find islands

--- src/hashMapAndTreeMap/Islands.java
+++ src/hashMapAndTreeMap/Islands.java
@@ -1,8 +1,12 @@
 package hashMapAndTreeMap;
 
+import unionFind.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Islands {
 
-    public static int countIslands(int[][] matrix) {
+    public static int countIslands1(int[][] matrix) {
         if (matrix == null || matrix[0] == null){
             return 0;
         }
@@ -36,12 +40,81 @@ public class Islands {
         infect(matrix, i, j - 1, row, column);
     }
 
-    public static class Element<V> {
-        public V value;
-
-        public Element(V value) {
-            this.value = value;
+    public static int countIslands2(int[][] matrix) {
+        List<String> list = new ArrayList<>();
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[0].length; column++) {
+                if (matrix[row][column] == 1) {
+                    String position = String.valueOf(row) + "_" + String.valueOf(column);
+                    list.add(position);
+                }
+            }
         }
+
+        UnionFind.UnionFindSet<String> unionSet = new UnionFind.UnionFindSet<>(list);
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[0].length; column++) {
+                if (matrix[row][column] == 1) {
+                    String position = String.valueOf(row) + "_" + String.valueOf(column);
+                    if (row - 1 >= 0 && matrix[row - 1][column] == 1) {
+                        String up = String.valueOf(row - 1) + "_" + String.valueOf(column);
+                        unionSet.union(up, position);
+                    }
+                    if (column - 1 >= 0 && matrix[row][column - 1] == 1) {
+                        String left = String.valueOf(row) + "_" + String.valueOf(column - 1);
+                        unionSet.union(left, position);
+                    }
+
+                }
+            }
+        }
+        return unionSet.getSetNum();
+    }
+
+    public static void main(String[] args) {
+        int[][] m1 = { { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 1, 1, 0, 1, 1, 1, 0 }, { 0, 1, 1, 1, 0, 0, 0, 1, 0 },
+                { 0, 1, 1, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 1, 0, 0 }, { 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+        for (int[] row : m1) {
+            for (int i : row) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(countIslands1(m1));
+
+        int[][] m1Other = { { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 1, 1, 0, 1, 1, 1, 0 }, { 0, 1, 1, 1, 0, 0, 0, 1, 0 },
+                { 0, 1, 1, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 1, 0, 0 }, { 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+        for (int[] row : m1Other) {
+            for (int i : row) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(countIslands2(m1Other));
+
+        int[][] m2 = { { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1, 1, 1, 1, 0 }, { 0, 1, 1, 1, 0, 0, 0, 1, 0 },
+                { 0, 1, 1, 0, 0, 0, 1, 1, 0 }, { 0, 0, 0, 0, 0, 1, 1, 0, 0 }, { 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+        for (int[] row : m2) {
+            for (int i : row) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(countIslands1(m2));
+
+        int[][] m2Other = { { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 1, 1, 1, 1, 1, 1, 0 }, { 0, 1, 1, 1, 0, 0, 0, 1, 0 },
+                { 0, 1, 1, 0, 0, 0, 1, 1, 0 }, { 0, 0, 0, 0, 0, 1, 1, 0, 0 }, { 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
+        for (int[] row : m2Other) {
+            for (int i : row) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(countIslands2(m2Other));
 
     }
 }
--- src/unionFind/UnionFind.java
+++ src/unionFind/UnionFind.java
@@ -63,5 +63,9 @@ public class UnionFind {
                 }
             }
         }
+
+        public int getSetNum() {
+            return sizeMap.size();
+        }
     }
 }
