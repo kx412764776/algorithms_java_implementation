b45735dbefc437fcbcdaef172d491d2bb87d33e2
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-12-01 02:40:28 +0000

    union find

---
+++ src/unionFind/UnionFind.java
@@ -0,0 +1,67 @@
+package unionFind;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
+public class UnionFind {
+
+    public static class Element<V> {
+        public V value;
+
+        public Element(V value) {
+            this.value = value;
+        }
+    }
+
+    public static class UnionFindSet<V> {
+        public HashMap<V, Element<V>> elementMap;
+        public HashMap<Element<V>, Element<V>> fatherMap;
+        public HashMap<Element<V>, Integer> sizeMap;
+
+        public UnionFindSet(List<V> list) {
+            elementMap = new HashMap<>();
+            fatherMap = new HashMap<>();
+            sizeMap = new HashMap<>();
+            for (V value : list) {
+                Element<V> element = new Element<V>(value);
+                elementMap.put(value, element);
+                fatherMap.put(element, element);
+                sizeMap.put(element, 1);
+            }
+        }
+
+        private Element<V> findHead(Element<V> element) {
+            Stack<Element<V>> path = new Stack<>();
+            while (element != fatherMap.get(element)) {
+                path.push(element);
+                element = fatherMap.get(element);
+            }
+            while (!path.isEmpty()) {
+                fatherMap.put(path.pop(), element);
+            }
+            return element;
+        }
+
+        public boolean isSameSet(V a, V b) {
+            if (elementMap.containsKey(a) && elementMap.containsKey(b)) {
+                return findHead(elementMap.get(a)) == findHead(elementMap.get(b));
+            }
+            return false;
+        }
+
+        public void union(V a, V b) {
+            if (elementMap.containsKey(a) && elementMap.containsKey(b)) {
+                Element<V> aHead = findHead(elementMap.get(a));
+                Element<V> bHead = findHead(elementMap.get(b));
+                if (aHead != bHead) {
+                    Element<V> big = sizeMap.get(aHead) >= sizeMap.get(bHead) ? aHead : bHead;
+                    Element<V> small = big == aHead ? bHead : aHead;
+                    fatherMap.put(small, big);
+                    sizeMap.put(big, sizeMap.get(aHead) + sizeMap.get(bHead));
+                    sizeMap.remove(small);
+                }
+            }
+        }
+    }
+}
